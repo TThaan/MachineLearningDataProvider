355719147a0d7e9e46bb3a6bebe70d20854c1770
TThaan <tthaan@gmx.de>
2021-07-24 10:37:57 +0200

    removed Initializer and made SampleSet the entry point.

--- DeepLearningDataProviderSolution/DeepLearningDataProvider/Sample.cs
+++ DeepLearningDataProviderSolution/DeepLearningDataProvider/Sample.cs
@@ -1,24 +1,8 @@
-﻿using Microsoft.ML.Data;
-
-namespace DeepLearningDataProvider
+﻿namespace DeepLearningDataProvider
 {
-    // How to have dynamic size of a vector property:
-    // https://github.com/dotnet/machinelearning/issues/164#issuecomment-401200501
-    // or: https://stackoverflow.com/a/24413055/10547243
-
-    // [Serializable]
     // Corresponds to 'InputData' and 'OutputData' in a standard MLNet code.
     public class Sample
     {
-        //private static int _x;
-
-        //public Sample(int x)
-        //{
-        //    _x = x;
-        //}
-
-        //[LoadColumn(_x)]
-        //[LoadColumn(0)]
         public string Label { get; set; }
         public float[] Features { get; set; }
         // public float[] Target { get; set; } // redundant?!
--- DeepLearningDataProviderSolution/DeepLearningDataProvider/SampleSet.cs
+++ DeepLearningDataProviderSolution/DeepLearningDataProvider/SampleSet.cs
@@ -1,31 +1,152 @@
-﻿using Microsoft.ML;
+﻿using DeepLearningDataProvider.Builders;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DeepLearningDataProvider
 {
     public interface ISampleSet
     {
-        //ISampleSetParameters Parameters { get; set; }
-        //float[][] TestingSamples { get; set; }
-        //float[][] TrainingSamples { get; set; }
-        //float[][] TestingTargets { get; set; }
-        //float[][] TrainingTargets { get; set; }
         Sample[] TestSet { get; set; }
         Sample[] TrainSet { get; set; }
-        //Sample[] TestTargets { get; set; }
-        //Sample[] TrainTargets { get; set; }
+        PathBuilder PathBuilder { get; }
+
+        Task<bool> LoadSampleSetAsync(string samplesFileName, float testSamplesFraction, int columnIndex_Label, params int[] ignoredColumnIndeces);
+        Task<bool> SaveSampleSetAsync(bool overWriteExistingFile = false);
+
+        event DataProviderChangedEventHandler DataProviderChanged;
     }
 
-    //[Serializable]
-    //[JsonObject()]
     public class SampleSet : ISampleSet
     {
-        //public float[][] TestingSamples { get; set; }
-        //public float[][] TrainingSamples { get; set; }
-        //public float[][] TestingTargets { get; set; }
-        //public float[][] TrainingTargets { get; set; }
+        #region fields & ctor
+
+        private PathBuilder pathBuilder;
+        private ISampleSet sampleSet;
+
+        public SampleSet()
+        {
+            pathBuilder = new PathBuilder(OnDataProviderChanged);   // via DC?
+        }
+
+        #endregion
+
+        #region properties
+
         public Sample[] TestSet { get; set; }
         public Sample[] TrainSet { get; set; }
-        //public Sample[] TestTargets { get; set; }
-        //public Sample[] TrainTargets { get; set; }
+        public PathBuilder PathBuilder
+        {
+            get
+            {
+                if (pathBuilder == null)
+                    OnDataProviderChanged("Paths are null");
+                return pathBuilder;
+            }
+        }        
+
+        #endregion
+
+        #region methods
+
+        public async Task<bool> LoadSampleSetAsync(string samplesFileName, float testSamplesFraction, int columnIndex_Label, params int[] ignoredColumnIndeces)//, int columnIndex_Target = -1
+        {
+            return await Task.Run(async () =>
+            {
+                try
+                {
+                    OnDataProviderChanged("Loading samples from file, please wait...");
+
+                    IEnumerable<Sample> samples = await InterpretFileAsync(samplesFileName, columnIndex_Label);
+
+                    int trainSamplesCount = (int)Math.Round(samples.Count() * (1 - testSamplesFraction), 0);
+                    TrainSet = samples.Take(trainSamplesCount).ToArray();
+                    TestSet = samples.Skip(trainSamplesCount).ToArray();
+
+                    OnDataProviderChanged("Successfully loaded samples.");
+                    return true;
+                }
+                catch (Exception e) { OnDataProviderChanged(e.Message); return false; }
+            });
+        }
+        public async Task<bool> SaveSampleSetAsync(bool overWriteExistingFile = false)
+        {
+            if (TrainSet == null)
+            { OnDataProviderChanged("There are no training samples loaded in SampleSet."); return false; }
+            if (TestSet == null)
+            { OnDataProviderChanged("There are no testing samples loaded in SampleSet."); return false; }
+
+            try
+            {
+                OnDataProviderChanged("Saving sample set, please wait...");
+
+                await ImpEx.Export.SaveAsCSVAsync(
+                    TrainSet.Concat(TestSet),
+                    PathBuilder.SampleSet,
+                    TrainSet.First().Features.Length + 1,
+                    overWriteExistingFile);
+
+                OnDataProviderChanged("Successfully saved sample set.");
+                return true;
+            }
+            catch (Exception e) { OnDataProviderChanged(e.Message); return false; }
+        }
+
+        #region helpers
+
+        /// <summary>
+        /// Currently only csv is loaded. More is coming.
+        /// </summary>
+        private async Task<IEnumerable<Sample>> InterpretFileAsync(string samplesFileName, int columnIndex_Label)
+        {
+            return await Task.Run(() =>
+            {
+                IEnumerable<string> lines = File.ReadLines(samplesFileName);
+                List<Sample> result = new List<Sample>(lines.Count());
+
+                // Get amount of feature columns
+
+                int featureColumnsCount = lines.First().Where(x => x == ',').Count();
+
+                // Get Samples (i.e. convert line to Sample?)
+                foreach (var line in lines)
+                {
+                    List<float> features = new List<float>(featureColumnsCount);
+
+                    Sample newSample = new Sample { Label = null, Features = new float[featureColumnsCount] };
+                    result.Add(newSample);
+
+                    string[] columns = line.Split(',');
+                    for (int i = 0; i < columns.Length; i++)
+                    {
+                        if (i == columnIndex_Label)
+                            newSample.Label = columns[i];
+                        else
+                            features.Add(float.Parse(columns[i], CultureInfo.InvariantCulture));
+                    }
+
+                    newSample.Features = features.ToArray();
+                }
+
+                return result;
+            });
+        }
+
+        #endregion
+
+        #endregion
+
+        #region DataProviderEventHandler
+
+        public event DataProviderChangedEventHandler DataProviderChanged;
+        void OnDataProviderChanged(string info)
+        {
+            DataProviderChanged?.Invoke(this, new DataProviderChangedEventArgs(info));
+        }
+
+        #endregion
     }
 }
